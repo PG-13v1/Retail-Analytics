8eb61b1b394b5a393c370ecbb0d002fc550eeb9c
PG-13 <working.pratul@gmail.com>
2026-02-26 14:56:59 +0530

    Update data loading to use Google Drive links

--- src/instacart/loader.py
+++ src/instacart/loader.py
@@ -2,9 +2,9 @@ import pandas as pd
 
 
 def load_instacart():
-    orders = pd.read_csv("data/instacart/orders.csv")
-    products = pd.read_csv("data/instacart/products.csv")
-    order_products = pd.read_csv("data/instacart/order_products__train.csv")
+    orders = pd.read_csv("https://drive.google.com/file/d/18EJoLq1tl0gwpHkn4Iv099ctaczZWlYK/view?usp=sharing")
+    products = pd.read_csv("https://drive.google.com/file/d/1UDJocoMhzZZFYEdq61pii7epE1g0j5Fi/view?usp=sharing")
+    order_products = pd.read_csv("https://drive.google.com/file/d/19DUKQWkLh6f4BqsgoBoTbwiUF5w7nscp/view?usp=sharing")
 
     merged = order_products.merge(products, on="product_id")
     return merged
