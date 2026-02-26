f50f479156655c42428a4d715654943458ddf071
PG-13 <working.pratul@gmail.com>
2026-02-26 15:05:53 +0530

    Change Walmart sales data source to Google Drive

--- src/walmart/loader.py
+++ src/walmart/loader.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 
 def load_walmart_sales():
-    df = pd.read_csv("data/walmart/train.csv")
+    df = pd.read_csv("https://drive.google.com/file/d/1qElZAoD8f78OcpVoKiz_YAot-Ft93jtj/view?usp=sharing")
     df["Date"] = pd.to_datetime(df["Date"])
     return df
