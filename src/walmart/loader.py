73a935831350507f5c9f2836ad2c61b1c17d340f
PG-13 <working.pratul@gmail.com>
2026-03-07 14:50:47 +0530

    Update loader.py

--- src/walmart/loader.py
+++ src/walmart/loader.py
@@ -1,7 +1,16 @@
 import pandas as pd
+import os
 
 
 def load_walmart_sales():
-    df = pd.read_csv("https://drive.google.com/file/d/1qElZAoD8f78OcpVoKiz_YAot-Ft93jtj/view?usp=sharing")
+    HF_TOKEN = os.getenv("HF_TOKEN")
+    url = "https://huggingface.co/datasets/PratulG/orders/resolve/main/train.csv"
+
+    df = pd.read_csv(
+    url,
+    storage_options={
+        "Authorization": f"Bearer {HF_TOKEN}"
+    })
+
     df["Date"] = pd.to_datetime(df["Date"])
     return df
